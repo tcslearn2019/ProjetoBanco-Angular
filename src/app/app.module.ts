56a44268190e00737aec5cbd72606b0846c4519a
Caroline Correa <1744958@TCS.com>
2019-09-12 16:29:07 -0300

    Adicionando arquivos de cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CadastroFormComponent } from './components/cadastro-form/cadastro-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroFormComponent
   ],
   imports: [
     BrowserModule
