75f6aa6470e42b078fd156584ffd0a137870e96b
Glauco Domiingos <glauco.domingues@hotmail.com>
2019-09-12 17:48:34 -0300

    Adicionado componente principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes} from '@angular/router'
 import { AppComponent } from './app.component';
 import { CadastroFormComponent } from './components/cadastro-form/cadastro-form.component';
+import { PrincipalFormComponent } from './components/principal-form/principal-form.component';
+
+
+const appRoutes:Routes = [
+  {path:'', component: CadastroFormComponent},
+  {path: 'pr', component: PrincipalFormComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CadastroFormComponent
+    CadastroFormComponent,
+    PrincipalFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
