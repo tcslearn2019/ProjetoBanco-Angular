56a2a0244af1d482c955d10c5e5471382009f2fd
Caroline Correa <1744958@TCS.com>
2019-09-13 11:47:37 -0300

    Adionando Cadastro e atualizando login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,22 @@ import { RouterModule, Routes} from '@angular/router'
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { PrincipalFormComponent } from './components/principal-form/principal-form.component';
+import { CadastroFormComponent } from './components/cadastro-form/cadastro-form.component';
 
 
 const appRoutes:Routes = [
-  {path:'', component: LoginFormComponent},
-  {path: 'pr', component: PrincipalFormComponent}
+  {path: '', component: LoginFormComponent},
+  {path: 'pr', component: PrincipalFormComponent},
+  {path: 'cadastro', component: CadastroFormComponent},
+
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginFormComponent,
-    PrincipalFormComponent
+    PrincipalFormComponent,
+    CadastroFormComponent,
   ],
   imports: [
     BrowserModule,
