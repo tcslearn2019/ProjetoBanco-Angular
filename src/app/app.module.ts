e1c96f0dc20decd359eadd0542ab6f164b1117ba
Glauco Domiingos <glauco.domingues@hotmail.com>
2019-09-13 09:42:37 -0300

    Aparecendo tela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router'
 import { AppComponent } from './app.component';
-import { CadastroFormComponent } from './components/cadastro-form/cadastro-form.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 import { PrincipalFormComponent } from './components/principal-form/principal-form.component';
 
 
 const appRoutes:Routes = [
-  {path:'', component: CadastroFormComponent},
+  {path:'', component: LoginFormComponent},
   {path: 'pr', component: PrincipalFormComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CadastroFormComponent,
+    LoginFormComponent,
     PrincipalFormComponent
   ],
   imports: [
