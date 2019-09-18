fcfac92eed30fd44b6d09d80408b340415dc15e4
Glauco Domiingos <glauco.domingues@hotmail.com>
2019-09-18 16:11:07 -0300

    Header

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -19,7 +19,7 @@ export class LoginFormComponent implements OnInit {
   }
   
   enviarLogin(){
-    this._userService.sendInformation(this.login).subscribe((login)=>{
+      this._userService.sendInformation(this.login).subscribe((login)=>{
       console.log(login.fname);
       this._userService.setter(login);
       this._router.navigate(['/principal']);
